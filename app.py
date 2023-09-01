9dddff559dd896fe8379786374a5cf440dc539be
Gill-Singh-A <amansg22@iitk.ac.in>
2023-09-02 00:53:07 +0530

    Implemented CORS

--- app.py
+++ app.py
@@ -3,8 +3,19 @@
 from algorithm import solution
 from fastapi import FastAPI
 from pydantic import BaseModel, Field
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 class Env(BaseModel):
     points: list
